62936011775e4b61f2a98e1c035813c366823c87
Giulidia <giulidia@gmail.com>
2022-10-12 16:23:41 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './styles/global.css';
 
 export function App() {
   return (
-    <h1>Hello Witches</h1>
+    <h1 className= "font-bold text-5xl text-violet-600 ">Hello Witches</h1>
   )
 }
 
