5fb8f7be2946d62f3245837c326875a9de420f1b
Giulidia <giulidia@gmail.com>
2022-10-13 10:05:45 +0100

    k

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,15 @@ module.exports = {
     './src/**/*.tsx',
   ],
   theme: {
+    colors: {
+      'gray-900': '#121214',
+      'gray-800': '#202024',
+      'gray-400': '#7c7c8a',
+      'gray-100': '#e1e1e6',
+      'gray-200': '#c4c4cc',
+      'cyan-500': '#81d8f7',
+      'cyan-300': '#9be1fb',
+    }
     extend: {
       fontFamily: {
         sans: 'Inter, sans-serif'
