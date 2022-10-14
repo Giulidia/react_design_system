2b54a07fdf20b82938bdbf1c47e75940d833660b
Giulidia <giulidia@gmail.com>
2022-10-14 16:24:42 +0100

    m

---
+++ storybook-static/assets/preview.49c96bba.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.cb4e7388.js";import"./iframe.314c91d7.js";import"./es.map.constructor.e1d88cfe.js";import"./es.number.to-fixed.e29b2f05.js";import"./index.82dfb6d2.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.49c96bba.js.map
